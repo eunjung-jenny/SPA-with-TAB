a85052439efdcbfa3ce069d66fd8589299efce26
eunjung.jenny <jenny.it.kr@gmail.com>
2021-05-07 19:20:41 +0900

    Auth 페이지 ui 구현

---
+++ frontend/src/components/antd/MyButton.tsx
@@ -0,0 +1,6 @@
+import { Button, ButtonProps } from 'antd'
+import React from 'react'
+
+const MyButton = (props: ButtonProps) => <Button {...props} />
+
+export default MyButton
--- frontend/src/pages/Auth.tsx
+++ frontend/src/pages/Auth.tsx
@@ -1,5 +1,40 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useHistory } from 'react-router-dom'
+import styled from 'styled-components'
+import MyButton from '../components/antd/MyButton'
 
-const Auth: React.FC = () => <div>인증</div>
+const Container = styled.div`
+  ul {
+    list-style: none;
+    margin-top: 20px;
+    li {
+      margin-top: 10px;
+    }
+  }
+`
+
+const Auth: React.FC = () => {
+  const history = useHistory()
+
+  useEffect(() => {
+    history.push('/login')
+  }, [])
+
+  return (
+    <Container className="auth">
+      <ul>
+        <li>
+          <MyButton>내부자로 로그인</MyButton>
+        </li>
+        <li>
+          <MyButton>외부자로 로그인 (two factor x)</MyButton>
+        </li>
+        <li>
+          <MyButton>외부자로 로그인 (two factor o)</MyButton>
+        </li>
+      </ul>
+    </Container>
+  )
+}
 
 export default Auth
